f82ea771b453a9066e1d8d5b5083b27556fb0b27
Rohit025005 <rohitsarwadikar@gmail.com>
2025-07-30 12:26:42 +0530

    changes

--- backend/config/env.js
+++ backend/config/env.js
@@ -6,7 +6,7 @@ export const  MONGO_URI = process.env.MONGO_URI
 export const  JWT_SECRET = process.env.JWT_SECRET
 export const  MAILTRAP_TOKEN = process.env.MAILTRAP_TOKEN
 
-console.log("token ", process.env.MONGO_URI)  // <--- this is the token you're looking for
+
 
 export const {
     PORT,NODE_ENV,MAILTRAP_ENDPOINT
